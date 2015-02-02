387dae9dd26be054174cbd4c189f5ef094f82fa5
Francisco Quesada Lara <Francisco.Quesada.Dev@gmail.com>
2015-02-02 08:40:45 -0500

    Redid entire project to simplify databases

--- SmartRoomSolution/SmartRoom.Database/dbo/Tables/ClassroomOptions.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ClassroomOptions] (
-    [Id]           INT            IDENTITY (1, 1) NOT NULL,
-    [CreateDate]   DATETIME       NOT NULL,
-    [CreatedBy]    NVARCHAR (MAX) NOT NULL,
-    [ModifiedDate] DATETIME       NOT NULL,
-    [ModifiedBy]   NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_ClassroomOptions] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
