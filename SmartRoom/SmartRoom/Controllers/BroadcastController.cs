482b55e154d9a8e43bedffeb6ddf6e5003a6e937
Francisco Quesada Lara <Francisco.Quesada.Dev@gmail.com>
2015-03-12 10:40:41 -0400

    this might work

--- SmartRoom/SmartRoom/Controllers/BroadcastController.cs
+++ SmartRoom/SmartRoom/Controllers/BroadcastController.cs
@@ -23,40 +23,60 @@ using Google.Apis.Auth.OAuth2.Flows;
 using Google.Apis.Auth.OAuth2.Requests;
 using Google.Apis.Auth.OAuth2.Responses;
 using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace SmartRoom.Web.Controllers
 {
     public class BroadcastController : Controller
     {
-        private readonly static string serviceAccountEmail = "1084733801830-qg01saqaeoicgtgv5c6l6qlduotsvnsu@developer.gserviceaccount.com";
+        //private readonly static string serviceAccountEmail = "1084733801830-qg01saqaeoicgtgv5c6l6qlduotsvnsu@developer.gserviceaccount.com";
 
-        private readonly static string[] scopes = new string[] {
-                //YouTubeService.Scope.Youtube,
-                YouTubeService.Scope.YoutubeReadonly,
-                YouTubeService.Scope.YoutubeUpload
-             };
+        //private readonly static string[] scopes = new string[] {
+        //        YouTubeService.Scope.Youtube
+        //     };
 
-        private readonly static X509Certificate2 certificate = new X509Certificate2("C:\\Users\\jquesad\\Documents\\GitHub\\SmartRoom\\SmartRoom\\SmartRoom\\SmartRoom-97a44346405c.p12", "notasecret", X509KeyStorageFlags.Exportable);
-        private readonly static ServiceAccountCredential credential = new ServiceAccountCredential(
-            new ServiceAccountCredential.Initializer(serviceAccountEmail)
-            {
-                Scopes = scopes
-            }.FromCertificate(certificate));
-        private readonly static YouTubeService youtube = new YouTubeService(new BaseClientService.Initializer()
-        {
+        //private readonly static X509Certificate2 certificate = new X509Certificate2("C:\\Users\\jquesad\\Documents\\GitHub\\SmartRoom\\SmartRoom\\SmartRoom\\SmartRoom-97a44346405c.p12", "notasecret", X509KeyStorageFlags.Exportable);
+        //private readonly static UserCredential credential = new UserCredential(
+        //    new UserCredential
+        //    {
+        //        Scopes = scopes
+        //    }.FromCertificate(certificate));
+        //private readonly static YouTubeService youtube = new YouTubeService(new BaseClientService.Initializer()
+        //{
             
-            HttpClientInitializer = credential,
-            ApiKey = "AIzaSyDDnEMgDDPgQm1mOSktMciDsbnLq41rHcQ"
-        });
+        //    HttpClientInitializer = credential,
+        //    ApiKey = "AIzaSyDDnEMgDDPgQm1mOSktMciDsbnLq41rHcQ"
+        //});
 
         //private static YouTubeService youtube = new YouTubeService(new BaseClientService.Initializer()
         //{
         //    ApiKey = "AIzaSyDDnEMgDDPgQm1mOSktMciDsbnLq41rHcQ"
         //});
 
-        public LiveBroadcast createBroadcast(String kind, String snippetTitle, DateTime startTime, DateTime endTime, String privacyStatus)
+        public async Task<LiveBroadcast> createBroadcast(String kind, String snippetTitle, DateTime startTime, DateTime endTime, String privacyStatus)
         {
-            
+
+            UserCredential credential;
+            using (var stream = new FileStream("C:\\Users\\jquesad\\Documents\\GitHub\\SmartRoom\\SmartRoom\\SmartRoom\\client_secret_1084733801830-4j2fje2ku2b6tkpa4v9v6cbbt08jeiql.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    // This OAuth 2.0 access scope allows an application to upload files to the
+                    // authenticated user's YouTube channel, but doesn't allow other types of access.
+                    new[] { YouTubeService.Scope.YoutubeUpload },
+                    "stevenjc721@gmail.com",
+                    CancellationToken.None
+                );
+            }
+
+
+            var youtube = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "SmartRoom"
+            });
+
+
             LiveBroadcast broadcast = new LiveBroadcast();
 
             // Set broadcast Kind
@@ -82,7 +102,8 @@ namespace SmartRoom.Web.Controllers
         public LiveStream createStream(String kind, String snippetTitle, String CDNFormat, String CDNIngestionType)
         {
             LiveStream stream = new LiveStream();
-
+            
+            var youtube = new YouTubeService(new BaseClientService.Initializer());
             // Set stream kind
             stream.Kind = kind;
 
@@ -104,7 +125,8 @@ namespace SmartRoom.Web.Controllers
 
         public LiveBroadcast bindBroadcast(LiveBroadcast broadcast, LiveStream stream)
         {
-
+            
+            var youtube = new YouTubeService(new BaseClientService.Initializer());
             LiveBroadcastsResource.BindRequest liveBroadcastBind = youtube.LiveBroadcasts.Bind(broadcast.Id, "id,contentDetails");
             liveBroadcastBind.StreamId = stream.Id;
             LiveBroadcast returnedBroadcast = liveBroadcastBind.Execute();
