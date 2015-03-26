260aba49868ab0811fe3d113c076c71e668ce885
brandon1@bellsouth.net <brandon1@bellsouth.net>
2015-03-25 22:06:50 -0400

    v3

---
+++ SmartRoomSolution/SmartRoomSolution/Controllers/ClassroomController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SmartRoomSolution.Models;
+using SmartRoomSolution.Web.Models;
+
+namespace SmartRoomSolution.Controllers
+{
+    public class ClassroomController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Classroom/
+        public ActionResult Index()
+        {
+            return View(db.Classrooms.ToList());
+        }
+
+        // GET: /Classroom/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classroom classroom = db.Classrooms.Find(id);
+            if (classroom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classroom);
+        }
+
+        // GET: /Classroom/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Classroom/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Term,Year,Campus,CreateDate,CreatedBy,ModifiedDate,ModifiedBy,IsActive")] Classroom classroom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Classrooms.Add(classroom);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(classroom);
+        }
+
+        // GET: /Classroom/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classroom classroom = db.Classrooms.Find(id);
+            if (classroom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classroom);
+        }
+
+        // POST: /Classroom/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Term,Year,Campus,CreateDate,CreatedBy,ModifiedDate,ModifiedBy,IsActive")] Classroom classroom)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(classroom).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(classroom);
+        }
+
+        // GET: /Classroom/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classroom classroom = db.Classrooms.Find(id);
+            if (classroom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classroom);
+        }
+
+        // POST: /Classroom/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Classroom classroom = db.Classrooms.Find(id);
+            db.Classrooms.Remove(classroom);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
