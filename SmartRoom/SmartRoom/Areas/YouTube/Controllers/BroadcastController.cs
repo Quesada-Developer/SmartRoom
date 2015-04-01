01dd27188e0096b23890d6e2ef9e0e8fe3369b84
steven <stevenjc721@gmail.com>
2015-04-01 11:45:49 -0400

    NewComp

--- SmartRoom/SmartRoom/Areas/YouTube/Controllers/BroadcastController.cs
+++ SmartRoom/SmartRoom/Areas/YouTube/Controllers/BroadcastController.cs
@@ -6,9 +6,11 @@ using System.Collections.Generic;
 
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
+// 
 
 namespace SmartRoom.Web.Areas.YouTube.Controllers
 {
+    using tranRef = Google.Apis.YouTube.v3.LiveBroadcastsResource.TransitionRequest;
     public class BroadcastController : Controller
     {
 
@@ -17,6 +19,7 @@ namespace SmartRoom.Web.Areas.YouTube.Controllers
                         "https://www.googleapis.com/auth/plus.login" });
         private YouTubeService youtube;
 
+        // creates a broadcast verified
         public async Task<LiveBroadcast> createBroadcast(String kind, String snippetTitle, DateTime startTime, DateTime endTime, String privacyStatus)
         {
             youtube = new YouTubeService(await youtubeAuthen.getInitializer());
@@ -42,6 +45,7 @@ namespace SmartRoom.Web.Areas.YouTube.Controllers
             return returnedBroadcast;
         }
 
+        // creates a stream verified
         public async Task<LiveStream> createStream(String kind, String snippetTitle, String CDNFormat, String CDNIngestionType)
         {
             LiveStream liveStream = new LiveStream();
@@ -67,6 +71,7 @@ namespace SmartRoom.Web.Areas.YouTube.Controllers
             return returnedStream;
         }
 
+        // Binds a stream to a broadcast verified
         public async Task<LiveBroadcast> bindBroadcast(LiveBroadcast broadcast, LiveStream Livestream)
         {
             youtube = new YouTubeService(await youtubeAuthen.getInitializer());
@@ -80,52 +85,106 @@ namespace SmartRoom.Web.Areas.YouTube.Controllers
 
         }
 
-        public async Task<LiveBroadcast> updateBroadcast(LiveBroadcast broadcast)
+        // Redo
+        public async Task<LiveBroadcast> transitionBroadcast(LiveBroadcast broadcast, String streamId, tranRef.BroadcastStatusEnum broadcastStatusEnum)
         {
             youtube = new YouTubeService(await youtubeAuthen.getInitializer());
 
-           // LiveBroadcastsResource.UpdateRequest liveBroadcastUpdates = youtube.LiveBroadcasts.Update();
+            IList<LiveStream> streams = await listStream();
 
-          // LiveBroadcast returnedBroadcast = liveBroadcastUpdates.Execute();
-          //   returnedBroadcast.ContentDetails.EnableEmbed = true;
+            for (int i = 0; i < streams.Count; i++)
+            {
+                if (streams[i].Id == streamId && streams[i].Status.StreamStatus == "active")
+                {
+
+                    LiveBroadcastsResource.TransitionRequest liveBroadcastTransition = youtube.LiveBroadcasts.Transition(broadcastStatusEnum, broadcast.Id, "id,snippet,contentDetails,status");
+
+                    LiveBroadcast broadcastResponse = liveBroadcastTransition.Execute();
+
+                    return broadcastResponse;
+
+                }
+            }
 
             return broadcast;
 
         }
 
-        //public async Task<LiveBroadcast> transitionBroadcast(LiveBroadcast broadcast, String broadcastStatus)
-        //{
-        //    youtube = new YouTubeService(await youtubeAuthen.getInitializer());
-
-        //    LiveStreamsResource.ListRequest streamRequest = youtube.LiveStreams.List("id");
-        //    streamRequest.Mine = true;
-
-        //    LiveStreamListResponse streamResponse = streamRequest.Execute();
-        //    List<LiveStream> streamCheck = streamResponse.Items.;
-
-        //    if (broadcast.Status.Equals(broadcastStatus)) {
-        //        return broadcast;
-        //    }
-
-           // LiveBroadcastsResource.TransitionRequest liveBroadcastTransition = youtube.LiveBroadcasts.Transition();
-
-          //  LiveBroadcast returnedBroadcast = liveBroadcastTransition.Execute();
-      //      returnedBroadcast.ContentDetails.EnableEmbed = true;
-
-        //    return returnedBroadcast;
-
-     //   }
-
-        public async Task<LiveBroadcast> deleteBroadcast(LiveBroadcast broadcast)
+        // needs to be tested
+        public async Task<LiveBroadcast> updateBroadcast(String broadcastId, String snippetTitle, DateTime startTime, DateTime endTime, String privacyStatus)
         {
             youtube = new YouTubeService(await youtubeAuthen.getInitializer());
 
-          //  LiveBroadcastsResource.UpdateRequest liveBroadcastUpdates = youtube.LiveBroadcasts.Update();
+            IList<LiveBroadcast> broadcasts = await listBroadcast();
 
-          //  LiveBroadcast returnedBroadcast = liveBroadcastUpdates.Execute();
-          //  returnedBroadcast.ContentDetails.EnableEmbed = true;
+            for (int i = 0; i < broadcasts.Count; i++)
+            {
+                if (broadcasts[i].Id == broadcastId)
+                {
+                    LiveBroadcast updateBroadcast = new LiveBroadcast();
+
+                    // Set broadcast snippet 
+                    updateBroadcast.Snippet = new LiveBroadcastSnippet();
+                    updateBroadcast.Snippet.Title = snippetTitle;
+                    updateBroadcast.Snippet.ScheduledStartTime = startTime;
+                    updateBroadcast.Snippet.ScheduledEndTime = endTime;
+
+                    //Set broadcast status
+                    updateBroadcast.Status = new LiveBroadcastStatus();
+                    updateBroadcast.Status.PrivacyStatus = privacyStatus;
+
+                    LiveBroadcastsResource.UpdateRequest liveBroadcastUpdate = youtube.LiveBroadcasts.Update(updateBroadcast, "id, snippet,contentDetails,status");
+
+                    LiveBroadcast broadcastResponse = liveBroadcastUpdate.Execute();
+
+                    return broadcastResponse;
+
+                }
+            }
+            throw new System.ArgumentException("Parameter must belong to user and not be null", "Invalid");
+        }
+
+
+        // Redo
+        public async Task<String> deleteBroadcast(String broadcastId)
+        {
+            youtube = new YouTubeService(await youtubeAuthen.getInitializer());
+
+            
 
             return broadcast;
+
+        }
+
+        // Lists Streams belonging to authenticated user
+        public async Task<IList<LiveStream>> listStream()
+        {
+          youtube = new YouTubeService(await youtubeAuthen.getInitializer());
+
+          LiveStreamsResource.ListRequest streamRequest = youtube.LiveStreams.List("id,snippet");
+          streamRequest.Mine = true;
+
+          LiveStreamListResponse streamResponse = streamRequest.Execute();
+          
+          IList<LiveStream> streamList = streamResponse.Items;
+
+          return streamList;
+
+        }
+
+        // Lists broadcasts belonging to authenticated user
+        public async Task<IList<LiveBroadcast>> listBroadcast()
+        {
+            youtube = new YouTubeService(await youtubeAuthen.getInitializer());
+
+            LiveBroadcastsResource.ListRequest broadcastRequest = youtube.LiveBroadcasts.List("id,snippet");
+            broadcastRequest.Mine = true;
+
+            LiveBroadcastListResponse broadcastResponse = broadcastRequest.Execute();
+
+            IList<LiveBroadcast> broadcastList = broadcastResponse.Items;
+
+            return broadcastList;
 
         }
     }
