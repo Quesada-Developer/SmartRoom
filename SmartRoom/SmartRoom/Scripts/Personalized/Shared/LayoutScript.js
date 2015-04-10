681f09eb4ef720d9b3735cb478ab5b2a7e6b1457
Francisco Quesada Lara <Francisco.Quesada.Dev@gmail.com>
2015-04-09 20:57:35 -0400

    fixes to datetimepicker

---
+++ SmartRoom/SmartRoom/Scripts/Personalized/Shared/LayoutScript.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
+});
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(function () { startTime() }, 500);
+    document.getElementById('date').innerHTML = (new Date()).toDateString();
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
