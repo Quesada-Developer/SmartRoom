b8fb9d57fa45091c68c41394b16402cb96560cef
Francisco Quesada Lara <Francisco.Quesada.Dev@gmail.com>
2015-04-19 17:02:30 -0400

    Experimental Header

---
+++ SmartRoom/SmartRoom/Areas/Classroom/Controllers/HeaderController.cs
@@ -0,0 +1,26 @@
+﻿using SmartRoom.Web.App_Start;
+using SmartRoom.Web.Controllers;
+using SmartRoom.Web.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+
+namespace SmartRoom.Web.Areas.Classroom.Controllers
+{
+    public class HeaderController : Controller
+    {
+        private readonly SmartModel db = new SmartModel();
+        private readonly AccountController Account = new AccountController();
+
+        public ActionResult _Header()
+        {
+            string _UserId = User.Identity.GetUserId();
+            var list = db.Users.Find(_UserId);
+            List<Course> Courses = (list != null) ? list.CoursesByRole(CourseRole.owner).ToList() : new List<Course>();
+            return View(Courses);
+        }
+    }
+}
