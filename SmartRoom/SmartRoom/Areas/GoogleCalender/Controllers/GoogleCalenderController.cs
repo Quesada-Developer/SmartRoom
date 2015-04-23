ffd906eda66a472a79c7df0d041f431711d61d71
steven <stevenjc721@gmail.com>
2015-04-22 21:05:10 -0400

    googleCalender

---
+++ SmartRoom/SmartRoom/Areas/GoogleCalender/Controllers/GoogleCalenderController.cs
@@ -0,0 +1,50 @@
+﻿using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Google.Apis.Calendar.v3;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SmartRoom.Web.Areas.GoogleCalender.Controllers
+{
+    using SmartRoom.Web.App_Start;
+    using tranRef = Google.Apis.YouTube.v3.LiveBroadcastsResource.TransitionRequest;
+    public class GoogleCalenderController : Controller
+    {
+
+        private readonly Authen googleAuthen = new Authen(new[] {   
+                        "https://www.googleapis.com/auth/plus.login",
+                        "https://www.googleapis.com/auth/Calendar"
+        });
+        private CalendarService googlecalender;
+
+        // creates a broadcast verified
+        public async Task<LiveBroadcast> createBroadcast(String kind, String snippetTitle, DateTime startTime, DateTime endTime, String privacyStatus)
+        {
+            googlecalender = new CalendarService(await googleAuthen.getInitializer());
+
+            ca
+            LiveBroadcast broadcast = new LiveBroadcast();
+
+            // Set broadcast Kind
+            broadcast.Kind = kind;
+
+            // Set broadcast snippet 
+            broadcast.Snippet = new LiveBroadcastSnippet();
+            broadcast.Snippet.Title = snippetTitle;
+            broadcast.Snippet.ScheduledStartTime = startTime;
+            broadcast.Snippet.ScheduledEndTime = endTime;
+
+            //Set broadcast status
+            broadcast.Status = new LiveBroadcastStatus();
+            broadcast.Status.PrivacyStatus = privacyStatus;
+
+
+            LiveBroadcast returnedBroadcast = youtube.LiveBroadcasts.Insert(broadcast, "id,snippet,status").Execute();
+
+
+            return returnedBroadcast;
+        }
+
+}
---
+++ SmartRoom/SmartRoom/Areas/GoogleCalender/GoogleCalenderAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SmartRoom.Web.Areas.GoogleCalender
+{
+    public class GoogleCalenderAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GoogleCalender";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GoogleCalender_default",
+                "GoogleCalender/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
